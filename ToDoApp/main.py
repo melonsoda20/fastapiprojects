9ea5977a86e9f2be4ea94588cd5b5944bb477be0
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-16 16:11:32 +0700

    Add endpoint to retrieve user todo list

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -4,6 +4,7 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
+from auth import get_current_user, get_user_exception
 
 
 app = FastAPI()
@@ -33,6 +34,18 @@ class Todo(BaseModel):
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todos/user")
+async def read_all_by_user(
+    user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    if user is None:
+        raise get_user_exception()
+    return db.query(models.Todos)\
+        .filter(models.Todos.owner_id == user.get("id"))\
+        .all()
 
 
 @app.get("/todo/{todo_id}")
