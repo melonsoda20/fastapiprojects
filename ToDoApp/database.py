0d531e27751e0e178c8275696315a35a4acfd3e5
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-17 15:16:27 +0700

    Add DB connection

--- ToDoApp/database.py
+++ ToDoApp/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from config import db_settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = db_settings.db_conn
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
