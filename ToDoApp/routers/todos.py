6febd121ab501627bdc56fae98ab32c3a659126a
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-20 10:34:20 +0700

    Add prefix for the routing

--- ToDoApp/routers/todos.py
+++ ToDoApp/routers/todos.py
@@ -9,9 +9,17 @@ from .auth import get_current_user, get_user_exception
 import sys
 sys.path.append("..")
 
-router = APIRouter()
-
 models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter(
+    prefix="/todos",
+    tags=["todos"],
+    responses={
+        404: {
+            "description": "Not found"
+        }
+    }
+)
 
 
 def get_db():
@@ -38,7 +46,7 @@ async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
 
 
-@router.get("/todos/user")
+@router.get("/user")
 async def read_all_by_user(
     user: dict = Depends(get_current_user),
     db: Session = Depends(get_db)
@@ -50,7 +58,7 @@ async def read_all_by_user(
         .all()
 
 
-@router.get("/todo/{todo_id}")
+@router.get("/{todo_id}")
 async def read_todo(
     todo_id: int,
     user: dict = Depends(get_current_user),
