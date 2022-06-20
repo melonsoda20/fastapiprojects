d38435ed1829b690ef7734a26728afd8252d13bb
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-20 13:08:51 +0700

    Add token header as dependency for company endpoints

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
 from database import engine
 from routers import auth, todos
-from company import companyapis
+from company import companyapis, dependencies
 
 app = FastAPI()
 
@@ -14,6 +14,7 @@ app.include_router(
     companyapis.router,
     prefix="/companyapis",
     tags=["companyapis"],
+    dependencies=[Depends(dependencies.get_token_header)],
     responses={
         418: {
             "description": "Internal use only"
