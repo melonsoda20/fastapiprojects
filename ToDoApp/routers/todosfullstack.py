f032a643eac45ce72147e0ac686c2ed680c764e6
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-21 15:36:44 +0700

    Add edit functionality for todo from the HTML

--- ToDoApp/routers/todosfullstack.py
+++ ToDoApp/routers/todosfullstack.py
@@ -89,10 +89,45 @@ async def create_todo(
 
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
-async def edit_todo(request: Request):
+async def edit_todo(
+    request: Request,
+    todo_id: int,
+    db: Session = Depends(get_db)
+):
+    todo = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+
     return templates.TemplateResponse(
         "edit-todo.html",
         {
-            "request": request
+            "request": request,
+            "todo": todo
         }
+    )
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(
+    request: Request,
+    todo_id: int,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+    db: Session = Depends(get_db)
+):
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(
+        url="/todosfullstack",
+        status_code=status.HTTP_302_FOUND
     )
