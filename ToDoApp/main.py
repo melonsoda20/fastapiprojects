8097b36a7bb8b65a6db03582d38e57fc808b6820
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-21 12:56:25 +0700

    Add html page to make a fullstack app using FastAPI

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI, Depends
 import models
 from database import engine
-from routers import auth, todos, users, address
+from routers import auth, todos, users, address, todosfullstack, authfullstack
 from company import companyapis, dependencies
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
 app.include_router(address.router)
+app.include_router(todosfullstack.router)
+app.include_router(authfullstack.router)
 app.include_router(
     companyapis.router,
     prefix="/companyapis",
---
+++ ToDoApp/routers/todosfullstack.py
@@ -0,0 +1,64 @@
+from typing import Optional
+from fastapi import Depends, HTTPException, APIRouter, Request
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from .auth import get_current_user, get_user_exception
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+import sys
+sys.path.append("..")
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+router = APIRouter(
+    prefix="/todosfullstack",
+    tags=["todosfullstack"],
+    responses={
+        404: {
+            "description": "Not found"
+        }
+    }
+)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def read_all_by_user(request: Request):
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request
+        }
+    )
+
+
+@router.get("/add-todo", response_class=HTMLResponse)
+async def add_new_todo(request: Request):
+    return templates.TemplateResponse(
+        "add-todo.html",
+        {
+            "request": request
+        }
+    )
+
+
+@router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo(request: Request):
+    return templates.TemplateResponse(
+        "edit-todo.html",
+        {
+            "request": request
+        }
+    )
